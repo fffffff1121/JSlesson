fde23758aee01381ceba1920ac1d09d5f7a4e9ac
Filipp <filippstryukov@gmail.com>
2019-05-02 19:05:07 +0300

    Edit Lesson file and add ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+// practic job number 1
+
+var titleJS = prompt("Каково «официальное» название JavaScript?" , '');
+if(titleJS == "ECMAScript")
+{
+	alert("Верно!");
+}else{
+	alert("Не знаете? «ECMAScript»!");
+}
+
