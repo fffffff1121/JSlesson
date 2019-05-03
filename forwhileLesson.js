2b0625f2d49059dc75edb681cdf8e4d16966cdfe
Filipp <filippstryukov@gmail.com>
2019-05-03 19:33:01 +0300

    Add scripts and add a new practic file

---
+++ forwhileLesson.js
@@ -0,0 +1,38 @@
+//practic 1
+for(var i = 0 ; i <= 10; i++){
+    if(i % 2 == 1) continue;
+    document.write(i + "\n"); 
+}
+
+document.write( "<hr />" );
+
+//practic 2
+
+var i = 0;
+
+while(i < 3){
+    alert( "номер " + i + "!" );
+	i++;
+}
+
+//practic 3
+
+while(true){
+
+var num = prompt("Введите число больше 100" , '');
+if(num > 100) {
+	alert("good"); 
+	break;
+} 
+
+}
+
+//practic 4
+
+
+for(i = 0; i <= 10; i++){
+	if(i % 2 == 1){
+    document.write(i + "\n");
+	}
+	
+}
--- practic.js
+++ practic.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var name = prompt("Input name - " , '');
 
 if(name == null){
