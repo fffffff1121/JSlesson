27a8addfd873307fc84da3256679092948e71cdc
Filipp <filippstryukov@gmail.com>
2019-05-05 11:53:26 +0300

    Edit files

---
+++ UppercaseAndLength.js
@@ -0,0 +1,8 @@
+var hello = "Привет !";
+var num = 12.211315451;
+
+document.write(hello.length
+               + "<br/>" + 
+	           hello.toUpperCase() 
+	           + "<br/>" + 
+	           num.toFixed(2));
